2465800ba014400c204d45878d3d2dc84bf46235
Bnoah456 <109919263+Bnoah456@users.noreply.github.com>
2022-09-20 11:03:48 -0500

    09/20/2022

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const tl = new TimelineMax();
 tl.fromTo(hero,1, {height: "0%"}, {height: "80%", ease: Power2.easeInOut})
 
 .fromTo(hero, 1.2, {width: "100%"}, {width:"80%", ease: Power2.easeInOut})
+
 .fromTo(slider, 1.2, {x:"-100%"}, {x:"0%", ease: Power2.easeInOut},"-=1.2")
 
 .fromTo(headline, 0.5, {opacity:0, x:30}, {opacity: 1, x:0}, "-=0.5")
@@ -16,28 +17,34 @@ tl.fromTo(hero,1, {height: "0%"}, {height: "80%", ease: Power2.easeInOut})
 .fromTo(headline2, 1, {opacity: 0, x:10}, {opacity: 1, x:0}, "-=0.3");
 
 //added a toggle hamburger menu icon//////////////////////
+
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
 
-hamburger.addEventListener("click", () => {
+hamburger.addEventListener("click", () =>
+{
     hamburger.classList.toggle('active');
     navMenu.classList.toggle('active');
-})
+    }
+)
 
-document.querySelectorAll('.nav-link').forEach(n => n. addEventListener('click', () => {
+document.querySelectorAll('.nav-link').forEach(n => n. addEventListener ('click', () => 
+{
     hamburger.classList.remove('active');
     navMenu.classList.remove('active');
-}))
+        }
+    )
+)
 
 ////////////////////////////////////////////////////
 
 function scrollAppear()
 {
    // var section = document.querySelector('.section-3');
-    var middleText = document.querySelector('.middle-text');
+   
     var introText = document.querySelector('.intro-text');
 
-    var middlePosition = middleText.getBoundingClientRect().top;
+    
     var introPosition = introText.getBoundingClientRect().top;
 
     var screenPosition = window.innerHeight / 2.2;
@@ -50,15 +57,11 @@ function scrollAppear()
         introText.classList.remove('intro-appear');
     }
 
-    if(middlePosition < screenPosition){
-        middleText.classList.add('intro-appear');
-    }
-
-    if(middlePosition > screenPosition){
-        middleText.classList.remove("intro-appear");
-    }
+    
     
 }
 
-
 window.addEventListener('scroll', scrollAppear);
+
+/////////////////////////////////////////////////////
+
